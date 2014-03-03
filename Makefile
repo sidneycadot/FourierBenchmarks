b02d7a19b09987e77b07ad8a5566b371a9d0442c
Sidney Cadot <sidney@jigsaw.nl>
2014-03-03 18:31:57 +0100

    Added FFTW.

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 
 CXXFLAGS = -W -Wall -O3 -std=c++11
 
+LDLIBS = -lfftw3
+
 naive : naive.cc
 
 clean :
--- naive.cc
+++ naive.cc
@@ -5,6 +5,7 @@
 #include <complex>
 #include <cassert>
 #include <chrono>
+#include <fftw3.h>
 
 using namespace std;
 
@@ -21,7 +22,9 @@ using namespace std;
 //     In-place or not
 //
 //     Multithreading
-//
+//     Optimization (planning) level (FFTW)
+//     Preserve input or not
+
 // Implementations:
 //
 // CPU:
@@ -158,7 +161,7 @@ ostream & print_vector(ostream & os, const vector<T> & v)
     return os;
 }
 
-double test(const unsigned n, const unsigned repeats)
+double test_naive(const unsigned n, const unsigned repeats)
 {
     vector<complex<double>> v = mk_complex_test_vector(n);
 
@@ -178,13 +181,74 @@ double test(const unsigned n, const unsigned repeats)
     return duration / repeats;
 }
 
+double test_fftw(const unsigned n, const unsigned repeats)
+{
+    vector<complex<double>> v = mk_complex_test_vector(n);
+
+    fftw_complex * x = fftw_alloc_complex(n);
+    assert(x != nullptr);
+
+    fftw_complex * y = fftw_alloc_complex(n);
+    assert(y != nullptr);
+
+    fftw_plan plan = fftw_plan_dft_1d(n, x, y, FFTW_FORWARD, FFTW_ESTIMATE | FFTW_PRESERVE_INPUT);
+    assert(plan != nullptr);
+
+    // Initialize the input
+
+    // copy test data
+    for (unsigned i = 0; i < n; ++i)
+    {
+        x[i][0] = real(v[i]);
+        x[i][1] = imag(v[i]);
+    }
+
+    auto t1 = chrono::high_resolution_clock::now();
+
+    for (unsigned rep = 0; rep < repeats; ++rep)
+    {
+        fftw_execute(plan);
+    }
+
+    auto t2 = chrono::high_resolution_clock::now();
+
+    double duration = chrono::duration_cast<chrono::nanoseconds>(t2 - t1).count() / 1e9;
+
+    fftw_destroy_plan(plan);
+
+    fftw_free(y);
+    fftw_free(x);
+
+    fftw_cleanup();
+
+    return duration / repeats;
+}
+
+void show()
+{
+    const unsigned n = 8;
+
+    vector<complex<double>> v = mk_complex_test_vector(n);
+
+    print_vector(cout, v); cout << endl;
+
+    vector<complex<double>> fv(n);
+
+    slow_fourier_1d(v, fv);
+
+    print_vector(cout, fv); cout << endl;
+}
+
 int main()
 {
+    show();
+    return 0;
+
     unsigned rep = 1000;
 
-    for (unsigned n = 0; n < 1000; ++n)
+    for (unsigned n = 1; n < 10; ++n)
     {
-        double duration = test(n, rep);
+        double duration = test_fftw(n, rep);
 
         cout << n << "\t" << rep << "\t" << duration << endl;
 
