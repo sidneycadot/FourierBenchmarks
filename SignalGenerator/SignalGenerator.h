ed452539b645cf56662e382ba8ef5f875f2dc6ca
Sidney Cadot <sidney@jigsaw.nl>
2014-05-04 17:50:01 +0200

    Signal generator, basic version is now working.

---
+++ SignalGenerator/SignalGenerator.h
@@ -0,0 +1,193 @@
+
+///////////////////////
+// SignalGenerator.h //
+///////////////////////
+
+#ifndef SignalGenerator_h
+#define SignalGenerator_h
+
+#include <vector>
+#include <complex>
+#include <string>
+
+#include <mpc.h>
+
+const mpfr_rnd_t DEFAULT_MPFR_ROUNDINGMODE = MPFR_RNDN;
+const mpc_rnd_t  DEFAULT_MPC_ROUNDINGMODE  = MPC_RNDNN;
+
+class GaussianNoiseSignal
+{
+    public:
+
+        GaussianNoiseSignal(const std::string & seed_string, const mpfr_prec_t precision)
+        {
+            // Prepare seed
+
+            mpz_init(seed); // initialize and set to zero.
+            mpz_init(seed2);
+
+            for (unsigned i = 0; seed_string[i] != '\0'; ++i)
+            {
+                const unsigned c = static_cast<unsigned char>(seed_string[i]);
+
+                mpz_mul_ui(seed, seed, 256);
+                mpz_add_ui(seed, seed, c);
+            }
+
+            // Prepare "re" and "im" variables, used during random generation.
+
+            mpfr_init2(re, precision);
+            mpfr_init2(im, precision);
+
+            gmp_randinit_default(randstate);
+        }
+
+        ~GaussianNoiseSignal()
+        {
+            gmp_randclear(randstate);
+
+            mpfr_clear(im);
+            mpfr_clear(re);
+
+            mpz_clear(seed2);
+            mpz_clear(seed);
+        }
+
+        void operator () (mpc_t & rop, const std::vector<signed> & index)
+        {
+            // Use index to set up seed, so we're reproducible.
+
+            std::string index_string;
+            for (unsigned i = 0; i < index.size(); ++i)
+            {
+                index_string += ".";
+                index_string += std::to_string(index[i]);
+            }
+
+            mpz_set(seed2, seed);
+
+            for (unsigned i = 0; index_string[i] != '\0'; ++i)
+            {
+                const unsigned c = static_cast<unsigned char>(index_string[i]);
+
+                mpz_mul_ui(seed2, seed2, 256);
+                mpz_add_ui(seed2, seed2, c);
+            }
+
+            gmp_randseed(randstate, seed2);
+
+            mpfr_grandom(re, im, randstate, DEFAULT_MPFR_ROUNDINGMODE);
+
+            mpc_set_fr_fr(rop, re, im, DEFAULT_MPFR_ROUNDINGMODE);
+        }
+
+    private:
+
+        mpz_t seed;
+        mpz_t seed2;
+
+        gmp_randstate_t randstate;
+
+        mpfr_t re;
+        mpfr_t im;
+};
+
+class ZeroSignal
+{
+    public:
+
+        void operator () (mpc_t & rop, const std::vector<signed> & index)
+        {
+            (void)index; // unused
+
+            mpc_set_ui(rop, 0, DEFAULT_MPC_ROUNDINGMODE);
+        }
+};
+
+template <typename T>
+T mpfr_value(const mpfr_t & op);
+
+template <>
+float mpfr_value<float>(const mpfr_t & op)
+{
+    return mpfr_get_flt(op, DEFAULT_MPFR_ROUNDINGMODE);
+}
+
+template <>
+double mpfr_value<double>(const mpfr_t & op)
+{
+    return mpfr_get_d(op, DEFAULT_MPFR_ROUNDINGMODE);
+}
+
+template <>
+long double mpfr_value<long double>(const mpfr_t & op)
+{
+    return mpfr_get_ld(op, DEFAULT_MPFR_ROUNDINGMODE);
+}
+
+// Routines to map an mpc_t to a complex or real value.
+
+template <typename T>
+void map_value(T & rop, const mpc_t & op)
+{
+    rop = mpfr_value<T>(mpc_realref(op));
+}
+
+template <typename T>
+void map_value(std::complex<T> & rop, const mpc_t & op)
+{
+    rop = std::complex<T>(mpfr_value<T>(mpc_realref(op)), mpfr_value<T>(mpc_imagref(op)));
+}
+
+template <typename Function, typename T>
+void sample(Function & f, T * base, const std::vector<signed> & strides, const std::vector<unsigned> & dims, const mpfr_prec_t precision)
+{
+    mpc_t value;
+
+    mpc_init2(value, precision);
+
+    std::vector<int> index(dims.size());
+
+    long signed offset = 0;
+
+    for (;;) // loop over the elements
+    {
+        // "index" and "offset" are now valid.
+
+         // set value to f(index)
+
+        f(value, index);
+
+        map_value(base[offset], value);
+
+        // Proceed to next item.
+
+        int incdim = dims.size();
+
+        while ((--incdim) >= 0)
+        {
+            ++index[incdim];
+            offset += strides[incdim];
+
+            if (index[incdim] != dims[incdim])
+            {
+                break;
+            }
+
+            // Proceed to earlier dimension
+
+            index[incdim] = 0;
+            offset -= strides[incdim] * dims[incdim];
+        }
+
+        if (incdim < 0)
+        {
+            // This was the last element
+            break;
+        }
+    }
+
+    mpc_clear(value);
+}
+
+#endif // SignalGenerator_h
--- SignalGenerator/TestSignalGenerator.cc
+++ SignalGenerator/TestSignalGenerator.cc
@@ -3,273 +3,47 @@
 // TestSignalGenerator.cc //
 ////////////////////////////
 
-#include <cmath>
+#include "SignalGenerator.h"
+
 #include <iostream>
-#include <iomanip>
-#include <limits>
-#include <cassert>
-#include <string>
+#include <vector>
+#include <complex>
 
-#include <mpfr.h>
-
-const mpfr_rnd_t DEFAULT_MPFR_ROUNDINGMODE = MPFR_RNDN;
-
-#if 0
-const mpc_rnd_t  DEFAULT_MPC_ROUNDINGMODE  = MPC_RNDNN;
-#endif
-
-std::string to_string(const mpfr_t & x)
-{
-    char * str;
-    int mpfr_asprintf_result = mpfr_asprintf(&str, "%.6Re", x);
-    assert(mpfr_asprintf_result > 0);
-
-    std::string r = str;
-
-    mpfr_free_str(str);
-
-    return r;
-}
-
-void zero_signal(const mpfr_t & x, mpfr_t & y)
-{
-    // Name ........... : zero signal
-    // Period ......... : n/a
-    // DC offset ...... : 0
-    // RMS ............ : 0
-    // Range .......... : [ 0 ]
-
-    (void)x; // unused
-
-    mpfr_set_zero(y, +1); // y = +0
-}
-
-
-void dc_signal(const mpfr_t & x, mpfr_t & y)
-{
-    // Name ........... : DC signal
-    // Period ......... : n/a
-    // DC offset ...... : sqrt(2) / 2
-    // RMS ............ : sqrt(2) / 2
-    // Range .......... : [ sqrt(2) / 2 ]
-
-    (void)x; // unused
-
-    // y = sqrt(0.5);
-
-    mpfr_set_ui(y, 1, DEFAULT_MPFR_ROUNDINGMODE);
-    mpfr_div_ui(y, y, 2, DEFAULT_MPFR_ROUNDINGMODE);
-    mpfr_sqrt(y, y, DEFAULT_MPFR_ROUNDINGMODE);
-}
-
-void sine_signal(const mpfr_t & x, mpfr_t & y)
-{
-    // Name ........... : sine wave
-    // Period ......... : 2*pi
-    // DC offset ...... : 0
-    // RMS ............ : sqrt(1 /2)
-    // Range .......... : [ sqrt(2) / 2 ]
-
-    mpfr_sin(y, x, DEFAULT_MPFR_ROUNDINGMODE);
-}
-
-#if 0
-void square_signal(const double & x, double & y)
-{
-    // Name ........... : square wave
-    // Period ......... : 2*pi
-    // DC offset ...... : 0
-    // RMS ............ : sqrt(1 / 2)
-    // Range .......... : [ -sqrt(1 / 2), +sqrt(1 / 2) ]
-
-    double xnorm = x / (2 * M_PI);
-
-    double frac = xnorm - floor(xnorm);
-
-    y = sqrt(0.5) * ((frac < 0.5) ? +1 : -1);
-}
-
-void triangle_signal(const double & x, double & y)
-{
-    // Name ........... : triangle wave
-    // Period ......... : 2*pi
-    // DC offset ...... : 0
-    // RMS ............ : sqrt(2)/2
-    // Range .......... : [ -sqrt(3 / 2), +sqrt(3 / 2) ]
-
-    double xnorm = x / (2 * M_PI);
-
-    double frac = xnorm - floor(xnorm);
-
-    y = sqrt(6) * (frac - abs(frac - 0.25) + abs(frac - 0.75) - 0.5);
-}
-
-void sawtooth_signal(const double & x, double & y)
-{
-    // Name ........... : sawtooth wave
-    // Period ......... : 2*pi
-    // DC offset ...... : 0
-    // RMS ............ : sqrt(2)/2
-    // Range .......... : [ -C, +C ]
-
-    double xnorm = x / (2 * M_PI);
-
-    double frac = (xnorm - floor(xnorm));
-
-    y = sqrt(6) * (frac - 0.5);
-}
-
-void gaussian_signal(const double & x, double & y)
-{
-    // Name ........... : gaussian noise
-    // Period ......... : n/a
-    // DC offset ...... : 0
-    // RMS ............ : sqrt(2)/2
-    // Range .......... : <-inf, +inf>
-
-    (void)x;
-
-    double U = static_cast<double>(rand()) / RAND_MAX;
-    double V = static_cast<double>(rand()) / RAND_MAX;
-
-    double R1 = sqrt(-2 * log(U)) * cos(2 * M_PI * V);
-    //double R2 = sqrt(-2 * log(U)) * sin(2 * M_PI * V);
-
-    y = sqrt(0.5) * R1;
-}
-#endif
+#include <mpc.h>
 
 using namespace std;
 
-void get_info(const char * name, void f(const mpfr_t & x, mpfr_t & y), unsigned n, mpfr_prec_t precision)
+int main()
 {
-    mpfr_t min_value;
-    mpfr_t max_value;
-    mpfr_t sum1;
-    mpfr_t sum2;
-    mpfr_t x;
-    mpfr_t y;
-    mpfr_t mean;
-    mpfr_t stddevSquared;
-    mpfr_t stddev;
-    mpfr_t powerSquared;
-    mpfr_t power;
+    const unsigned SIZE = 256;
 
-    mpfr_init2(min_value     , precision);
-    mpfr_init2(max_value     , precision);
-    mpfr_init2(sum1          , precision);
-    mpfr_init2(sum2          , precision);
-    mpfr_init2(x             , precision);
-    mpfr_init2(y             , precision);
-    mpfr_init2(mean          , precision);
-    mpfr_init2(stddevSquared , precision);
-    mpfr_init2(stddev        , precision);
-    mpfr_init2(powerSquared  , precision);
-    mpfr_init2(power         , precision);
+    unsigned n = SIZE * SIZE;
 
-    mpfr_set_inf(min_value, +1); // +infinity
-    mpfr_set_inf(max_value, -1); // -infinity
+    const mpfr_prec_t precision = 1024;
 
-    mpfr_set_zero(sum1, +1); // +zero
-    mpfr_set_zero(sum2, +1); // +zero
+    vector<complex<float>> arr(n);
 
     for (unsigned i = 0; i < n; ++i)
     {
-        // x = (double)i / n * 2 * M_PI;
-
-        mpfr_const_pi(x, DEFAULT_MPFR_ROUNDINGMODE);
-        mpfr_mul_ui(x, x, 2 * i, DEFAULT_MPFR_ROUNDINGMODE);
-        mpfr_div_ui(x, x, n, DEFAULT_MPFR_ROUNDINGMODE);
-
-        f(x, y);
-
-        mpfr_min(min_value, min_value, y, DEFAULT_MPFR_ROUNDINGMODE);
-        mpfr_max(max_value, max_value, y, DEFAULT_MPFR_ROUNDINGMODE);
-
-        // sum1 += y
-
-        mpfr_add(sum1, sum1, y, DEFAULT_MPFR_ROUNDINGMODE);
-
-        // sum2 += y * y
-
-        mpfr_sqr(y, y, DEFAULT_MPFR_ROUNDINGMODE);
-        mpfr_add(sum2, sum2, y, DEFAULT_MPFR_ROUNDINGMODE);
+        arr[i] = -999.0;
     }
 
-    // double mean   = sum1 / n;
+    std::vector<unsigned> dims    = {SIZE, SIZE};
+    std::vector<signed  > strides = {SIZE, 1};
 
-    mpfr_div_ui(mean, sum1, n, DEFAULT_MPFR_ROUNDINGMODE);
+    GaussianNoiseSignal noise("", precision);
+    //ZeroSignal zero;
 
-    //double stddev = sqrt(abs(n * sum2 - sum1 * sum1)) / n; // The "abs" is there just to be sure we don't get to negative values due to roundoff.
-    // x = sum2/n
-    // y = mean*mean
-    // stddevSquared = x - y
+    sample(noise, arr.data(), strides, dims, precision);
 
-    mpfr_div_ui(x, sum2, n, DEFAULT_MPFR_ROUNDINGMODE);
-    mpfr_sqr(y, mean, DEFAULT_MPFR_ROUNDINGMODE);
-
-    mpfr_sub(stddevSquared, x, y, DEFAULT_MPFR_ROUNDINGMODE);
-
-    // Due to roundoff, this could be just below 0.
-    // Take the abs() to prevent this problem.
-    mpfr_abs(stddevSquared, stddevSquared, DEFAULT_MPFR_ROUNDINGMODE);
-
-    mpfr_sqrt(stddev, stddevSquared, DEFAULT_MPFR_ROUNDINGMODE);
-
-    // double power = sqrt(sum2 / n);
-
-    mpfr_div_ui(powerSquared, sum2, n, DEFAULT_MPFR_ROUNDINGMODE);
-
-    mpfr_sqrt(power, powerSquared, DEFAULT_MPFR_ROUNDINGMODE);
-
-    cout << "================= " << name << endl;
-    cout << endl;
-
-    cout << "min value ....... : " << to_string(min_value)       << endl;
-    cout << "max value ....... : " << to_string(max_value)       << endl;
-    cout << "n ............... : " << to_string(n)               << endl;
-    cout << "sum1 ............ : " << to_string(sum1)            << endl;
-    cout << "sum2 ............ : " << to_string(sum2)            << endl;
-    cout << "mean value ...... : " << to_string(mean)            << endl;
-    cout << "stddev .......... : " << to_string(stddev)          << endl;
-    cout << "stddev^2 ........ : " << to_string(stddevSquared)   << endl;
-    cout << "power ........... : " << to_string(power)           << endl;
-    cout << "power^2 ......... : " << to_string(powerSquared)    << endl;
-
-    cout << endl;
-
-    mpfr_clear( min_value     );
-    mpfr_clear( max_value     );
-    mpfr_clear( sum1          );
-    mpfr_clear( sum2          );
-    mpfr_clear( x             );
-    mpfr_clear( y             );
-    mpfr_clear( mean          );
-    mpfr_clear( stddevSquared );
-    mpfr_clear( stddev        );
-    mpfr_clear( powerSquared  );
-    mpfr_clear( power         );
-}
-
-int main()
-{
-    unsigned n = 100000;
-
-    mpfr_prec_t precision = 256;
-
-    get_info("zero_signal"     , zero_signal     , n, precision);
-    get_info("dc_signal"       , dc_signal       , n, precision);
-    get_info("sine_signal"     , sine_signal     , n, precision);
-    //get_info("square_signal"   , square_signal   , n);
-    //get_info("triangle_signal" , triangle_signal , n);
-    //get_info("sawtooth_signal" , sawtooth_signal , n);
-    //get_info("gaussian_signal" , gaussian_signal , n);
+    if (0)
+    for (unsigned i = 0; i < n; ++i)
+    {
+        cout << i << " " << arr[i] << endl;
+    }
 
     // Free cache of MPFR, used for PI.
     mpfr_free_cache();
-
-    // Free the GMP random state.
-    //gmp_randclear(rnd_state);
 
     return 0;
 }
