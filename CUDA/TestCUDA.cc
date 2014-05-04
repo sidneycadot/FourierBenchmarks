dca41a9a91e91bf122d86ea737f4459ddebaf8bd
Sidney Cadot <sidney@jigsaw.nl>
2014-05-04 12:54:40 +0200

    Updating TestCUDA.

--- CUDA/TestCUDA.cc
+++ CUDA/TestCUDA.cc
@@ -1,4 +1,8 @@
 
+/////////////////
+// TestCUDA.cc //
+/////////////////
+
 #include <cassert>
 #include <iostream>
 #include <vector>
@@ -9,73 +13,17 @@
 
 using namespace std;
 
-bool is_prime(unsigned n)
-{
-    if (n < 2)
-    {
-        return false;
-    }
-
-    for (unsigned d = 2; d * d <= n; ++d)
-    {
-        if (n % d == 0)
-        {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-vector<complex<double>> mk_complex_test_vector(unsigned n)
-{
-    vector<complex<double>> v;
-    v.reserve(n);
-
-    unsigned p = 0;
-    while (v.size() < n)
-    {
-        while (!is_prime(p))
-        {
-            ++p;
-        }
-
-        v.push_back(complex<double>(sqrt(p), cbrt(p)));
-
-        ++p;
-    }
-
-    return v;
-}
-
-template <typename T>
-ostream & print_vector(ostream & os, const vector<T> & v)
-{
-    os << "{";
-    for (unsigned i = 0; i < v.size(); ++i)
-    {
-        if (i != 0)
-        {
-            os << ", ";
-        }
-        os << v[i];
-    }
-    os << "}";
-
-    return os;
-}
-
 int main()
 {
     const unsigned n = 8;
 
-    vector<complex<double>> tvec = mk_complex_test_vector(n);
+    vector<complex<double> > tvec(n);
 
-    cout << "tvec: ";
-    print_vector(cout, tvec);
-    cout << endl;
+    //cout << "tvec: ";
+    //print_vector(cout, tvec);
+    //cout << endl;
 
-    vector<complex<double>> fvec(n);
+    vector<complex<double> > fvec(n);
 
     cudaError_t cudaErr;
 
@@ -109,9 +57,9 @@ int main()
     cudaErr = cudaMemcpy(fvec.data(), fdata_device, n * sizeof(cufftDoubleComplex), cudaMemcpyDeviceToHost);
     assert(cudaErr == cudaSuccess);
 
-    cout << "fvec: ";
-    print_vector(cout, fvec);
-    cout << endl;
+    //cout << "fvec: ";
+    //print_vector(cout, fvec);
+    //cout << endl;
 
     cufft_result = cufftDestroy(plan);
     assert(cufft_result == CUFFT_SUCCESS);
